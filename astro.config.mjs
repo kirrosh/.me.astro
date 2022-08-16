8ce26590bea65c084ac757093b975fd8dc123a35
Koshelev Kirill <olegartor7@gmail.com>
2022-08-16 08:56:36 +0300

    fix: adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import solidJs from "@astrojs/solid-js";
-import vercel from "@astrojs/vercel/edge";
+import vercel from "@astrojs/vercel/serverless";
 
 import tailwind from "@astrojs/tailwind";
 
