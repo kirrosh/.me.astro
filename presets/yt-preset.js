4f3cee3e2e3477db0e7e4ac4351d9d37c7bd9117
Koshelev Kirill <olegartor7@gmail.com>
2022-11-04 20:19:00 +0300

    feat(yt-post): added tw preset

---
+++ presets/yt-preset.js
@@ -0,0 +1,15 @@
+module.exports = {
+  theme: {
+    colors: {
+      "yt-primary": "#0f0f0f",
+      "yt-button": "#ffffff1a",
+      "yt-text": "#f1f1f1",
+    },
+    fontSize: {
+      yt: ["14px", "36px"],
+    },
+    borderRadius: {
+      yt: "18px;",
+    },
+  },
+};
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,7 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   darkMode: "class",
+  presets: [require("./presets/yt-preset.js")],
   plugins: [require("@tailwindcss/typography"), require("daisyui")],
   theme: {
     fontFamily: {
